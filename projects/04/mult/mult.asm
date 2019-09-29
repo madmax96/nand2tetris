bd454b35897f821236fa064271618c8e9a914fe3
madmax <mladjan96@gmail.com>
2019-09-29 21:45:02 +0200

    project 4: multiplication.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R1
+D=M
+@R2
+M=D // load second number in result memory R2
+@END
+D;JEQ // if second number is 0 then END
+
+@R0
+D=M
+@R2
+M=D // load first number in result memory R2
+@END
+D;JEQ // if first number is 0 then END
+
+(LOOP)
+    @R1
+    D=M-1
+    @END
+    D;JEQ
+    @R1
+    M=M-1 // decrease second num by 1
+    @R0
+    D=M
+    @R2
+    M=M+D // increase result by first num
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP

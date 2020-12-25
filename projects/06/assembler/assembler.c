8cc0c24ad94063fe96ce67109cb6130c12da42b2
madmax <mladjan96@gmail.com>
2020-12-25 20:26:42 +0100

    c coding standards changes

--- projects/06/assembler/assembler.c
+++ projects/06/assembler/assembler.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <inttypes.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <ctype.h>
 #include <string.h>
 #include <time.h>
@@ -8,17 +8,18 @@
 
 #define MAX_LINE 1000
 
-enum COMMAND_TYPE { A_COMMAND, C_COMMAND, LABEL };
-enum A_COMMAND_MODE { A_VALUE, A_SYMBOL };
+typedef uint8_t byte;
+
+enum INSTRUCTION_TYPE { A_INSTRUCTION, C_INSTRUCTION, LABEL };
+enum A_INSTRUCTION_MODE { A_VALUE, A_SYMBOL };
 int16_t rom_address = 0;
 int16_t ram_address = 16;
-
-typedef struct Command {
-    enum COMMAND_TYPE type;
-    enum A_COMMAND_MODE a_cmd_mode;
-    char* commandText;
+typedef struct instruction {
+    enum INSTRUCTION_TYPE type;
+    enum A_INSTRUCTION_MODE a_instr_mode;
+    char* instruction_text;
     union {
-        char* label; // if type is label
+        char* label;
         char* symbol;
         int16_t value;
         struct {
@@ -27,26 +28,22 @@ typedef struct Command {
             char* jump;
         } c_fields;
     } data;
-} Command;
-
-typedef struct C_NODE {
-    Command* cmd;
-    struct C_NODE* next;
-} C_NODE;
-
-Command* new_command() {
-    Command* c = malloc(sizeof(Command));
-    // c->data.symbol = NULL;
-    // c->data.label = NULL;
-    return c;
-}
+} instruction;
+typedef struct c_node {
+    instruction* instr;
+    struct c_node* next;
+} c_node;
 
 char* int_to_bin16(int16_t num);
+void assemble(c_node* head_instruction, Map m, char* dest);
 
-void assemble(C_NODE* head_command, Map m, char* dest);
+instruction* new_instruction()
+{
+    return (instruction*)malloc(sizeof(instruction));
+}
 
-void install_predefined_symbols(Map m){
-
+void install_predefined_symbols(Map m)
+{
     map_add(m,"SP",int_to_bin16(0));
     map_add(m,"LCL",int_to_bin16(1));
     map_add(m,"ARG",int_to_bin16(2));
@@ -74,163 +71,163 @@ void install_predefined_symbols(Map m){
     map_add(m,"KBD",int_to_bin16(24576));
 }
 
-char* get_comp_inst(char* comp_cmd) {
-    if (strcmp(comp_cmd, "0") == 0){
+char* get_comp_inst(char* comp_instr)
+{
+    if (strcmp(comp_instr, "0") == 0) {
         return "0101010";
     }
-    if (strcmp(comp_cmd, "1") == 0){
+    if (strcmp(comp_instr, "1") == 0) {
         return "0111111";
     }
-    if (strcmp(comp_cmd, "-1") == 0){
+    if (strcmp(comp_instr, "-1") == 0) {
         return "0111010";
     }
-    if (strcmp(comp_cmd, "D") == 0){
+    if (strcmp(comp_instr, "D") == 0) {
         return "0001100";
     }
-    if (strcmp(comp_cmd, "A") == 0){
+    if (strcmp(comp_instr, "A") == 0) {
         return "0110000";
     }
-    if (strcmp(comp_cmd, "!D") == 0){
+    if (strcmp(comp_instr, "!D") == 0) {
         return "0001101";
     }
-    if (strcmp(comp_cmd, "!A") == 0){
+    if (strcmp(comp_instr, "!A") == 0) {
         return "0110001";
     }
-    if (strcmp(comp_cmd, "-D") == 0){
+    if (strcmp(comp_instr, "-D") == 0) {
         return "0001111";
     }
-    if (strcmp(comp_cmd, "-A") == 0){
+    if (strcmp(comp_instr, "-A") == 0) {
         return "0110011";
     }
-    if (strcmp(comp_cmd, "D+1") == 0){
+    if (strcmp(comp_instr, "D+1") == 0) {
         return "0011111";
     }
-    if (strcmp(comp_cmd, "A+1") == 0){
+    if (strcmp(comp_instr, "A+1") == 0) {
         return "0110111";
     }
-    if (strcmp(comp_cmd, "D-1") == 0){
+    if (strcmp(comp_instr, "D-1") == 0) {
         return "0001110";
     }
-    if (strcmp(comp_cmd, "A-1") == 0){
+    if (strcmp(comp_instr, "A-1") == 0) {
         return "0110010";
     }
-    if (strcmp(comp_cmd, "D+A") == 0){
+    if (strcmp(comp_instr, "D+A") == 0) {
         return "0000010";
     }
-    if (strcmp(comp_cmd, "D-A") == 0){
+    if (strcmp(comp_instr, "D-A") == 0) {
         return "0010011";
     }
-    if (strcmp(comp_cmd, "A-D") == 0){
+    if (strcmp(comp_instr, "A-D") == 0) {
         return "0000111";
     }
-    if (strcmp(comp_cmd, "D&A") == 0){
+    if (strcmp(comp_instr, "D&A") == 0) {
         return "0000000";
     }
-    if (strcmp(comp_cmd, "D|A") == 0){
+    if (strcmp(comp_instr, "D|A") == 0) {
         return "0010101";
     }
-    if (strcmp(comp_cmd, "M") == 0){
+    if (strcmp(comp_instr, "M") == 0) {
         return "1110000";
     }
-    if (strcmp(comp_cmd, "!M") == 0){
+    if (strcmp(comp_instr, "!M") == 0) {
         return "1110001";
     }
-    if (strcmp(comp_cmd, "-M") == 0){
+    if (strcmp(comp_instr, "-M") == 0) {
         return "1110011";
     }
-    if (strcmp(comp_cmd, "M+1") == 0){
+    if (strcmp(comp_instr, "M+1") == 0) {
         return "1110111";
     }
-    if (strcmp(comp_cmd, "M-1") == 0){
+    if (strcmp(comp_instr, "M-1") == 0) {
         return "1110010";
     }
-    if (strcmp(comp_cmd, "D+M") == 0){
+    if (strcmp(comp_instr, "D+M") == 0) {
         return "1000010";
     }
-    if (strcmp(comp_cmd, "D-M") == 0){
+    if (strcmp(comp_instr, "D-M") == 0) {
         return "1010011";
     }
-    if (strcmp(comp_cmd, "M-D") == 0){
+    if (strcmp(comp_instr, "M-D") == 0) {
         return "1000111";
     }
-    if (strcmp(comp_cmd, "D&M") == 0){
+    if (strcmp(comp_instr, "D&M") == 0) {
         return "1000000";
     }
-    if (strcmp(comp_cmd, "D|M") == 0){
+    if (strcmp(comp_instr, "D|M") == 0) {
         return "1010101";
     }
     return "0101010";
 }
 
-char* get_dest_inst(char* dest_cmd) {
-
-    if(dest_cmd == NULL){
-        return "000";
-    }
-    if (strcmp(dest_cmd, "M") == 0){
-        return "001";
-    }
-    if (strcmp(dest_cmd, "D") == 0){
-        return "010";
-    }
-    if (strcmp(dest_cmd, "MD") == 0){
-        return "011";
-    }
-    if (strcmp(dest_cmd, "A") == 0){
-        return "100";
-    }
-    if (strcmp(dest_cmd, "AM") == 0){
-        return "101";
-    }
-    if (strcmp(dest_cmd, "AD") == 0){
-        return "110";
-    }
-    if (strcmp(dest_cmd, "AMD") == 0){
-        return "111";
-    }
-    return "000";
-}
-
-char* get_jmp_inst(char* jmp_cmd) {
-
-    if(jmp_cmd == NULL){
-        return "000";
-    }
-    if (strcmp(jmp_cmd, "JGT") == 0){
-        return "001";
-    }
-    if (strcmp(jmp_cmd, "JEQ") == 0){
-        return "010";
-    }
-    if (strcmp(jmp_cmd, "JGE") == 0){
-        return "011";
-    }
-    if (strcmp(jmp_cmd, "JLT") == 0){
-        return "100";
-    }
-    if (strcmp(jmp_cmd, "JNE") == 0){
-        return "101";
-    }
-    if (strcmp(jmp_cmd, "JLE") == 0){
-        return "110";
-    }
-    if (strcmp(jmp_cmd, "JMP") == 0){
-        return "111";
-    }
-    return "000";
-}
-
-C_NODE* list_add(C_NODE* head, Command* cmd)
+char* get_dest_inst(char* dest_instr)
 {
+    if (dest_instr == NULL) {
+        return "000";
+    }
+    if (strcmp(dest_instr, "M") == 0) {
+        return "001";
+    }
+    if (strcmp(dest_instr, "D") == 0) {
+        return "010";
+    }
+    if (strcmp(dest_instr, "MD") == 0) {
+        return "011";
+    }
+    if (strcmp(dest_instr, "A") == 0) {
+        return "100";
+    }
+    if (strcmp(dest_instr, "AM") == 0) {
+        return "101";
+    }
+    if (strcmp(dest_instr, "AD") == 0) {
+        return "110";
+    }
+    if (strcmp(dest_instr, "AMD") == 0) {
+        return "111";
+    }
+    return "000";
+}
 
-    C_NODE* new = malloc(sizeof(C_NODE));
-    new->cmd = cmd;
+char* get_jmp_inst(char* jmp_instr)
+{
+    if (jmp_instr == NULL) {
+        return "000";
+    }
+    if (strcmp(jmp_instr, "JGT") == 0) {
+        return "001";
+    }
+    if (strcmp(jmp_instr, "JEQ") == 0) {
+        return "010";
+    }
+    if (strcmp(jmp_instr, "JGE") == 0) {
+        return "011";
+    }
+    if (strcmp(jmp_instr, "JLT") == 0) {
+        return "100";
+    }
+    if (strcmp(jmp_instr, "JNE") == 0) {
+        return "101";
+    }
+    if (strcmp(jmp_instr, "JLE") == 0) {
+        return "110";
+    }
+    if (strcmp(jmp_instr, "JMP") == 0) {
+        return "111";
+    }
+    return "000";
+}
+
+c_node* list_add(c_node* head, instruction* instr)
+{
+    c_node* new = malloc(sizeof(c_node));
+    new->instr = instr;
     new->next = NULL;
     if (head == NULL)
     {
         return new;
     }
-    C_NODE* temp = head;
+    c_node* temp = head;
     while (temp->next != NULL)
     {
         temp = temp->next;
@@ -239,20 +236,22 @@ C_NODE* list_add(C_NODE* head, Command* cmd)
     return head;
 }
 
-void print_commands(C_NODE* head) {
-    C_NODE* temp = head;
+void print_instructions(c_node* head)
+{
+    c_node* temp = head;
     int i = 0;
-    while(temp != NULL){
-        printf("%d. %s\n", i, temp->cmd->commandText);
+    while (temp != NULL) {
+        printf("%d. %s\n", i, temp->instr->instruction_text);
         temp = temp->next;
         i++;
     }
 }
 
-char* int_to_bin16(int16_t num) {
+char* int_to_bin16(int16_t num)
+{
     int originalNUm = num;
     int i = 16; // number of bits
-    char* binary = malloc(i + 1);
+    char* binary = (char*) malloc(i + 1);
     binary[i] = '\0';
     while(i > 0) {
         binary[i-1] = (num & (int16_t)1) ? '1' : '0';
@@ -262,30 +261,30 @@ char* int_to_bin16(int16_t num) {
     return binary;
 }
 
-Command* parse_line (char line[], Map m) {
-
+instruction* parse_line (char line[], Map m)
+{
     int len = strlen(line);
     if (!len) return NULL;
-    Command* c = new_command();
-    c->commandText = malloc(strlen(line) + 1);
-    strcpy(c->commandText, line);
+    instruction* c = new_instruction();
+    c->instruction_text = malloc(strlen(line) + 1);
+    strcpy(c->instruction_text, line);
 
     if (line[0] == '@') {
-        c->type = A_COMMAND;
+        c->type = A_INSTRUCTION;
         if (isdigit(line[1]) || line[1] == '-') {
             char digits[6];
             for (int i = 1; i <= 6; i++) {
                 digits[i-1] = line[i];
             }
             c->data.value = atoi(digits);
-            c->a_cmd_mode = A_VALUE;
+            c->a_instr_mode = A_VALUE;
         } else {
             char* symbol = malloc(len);
             for (int i = 1; i <= len; i++) {
                 symbol[i - 1] = line[i];
             }
             c->data.symbol = symbol;
-            c->a_cmd_mode = A_SYMBOL;
+            c->a_instr_mode = A_SYMBOL;
         }
         rom_address++;
     } else if (line[0] == '(') {
@@ -298,13 +297,13 @@ Command* parse_line (char line[], Map m) {
         }
         label[len-1] = '\0';
         c->data.label = label;
-        if (map_get(m, label) != NULL){
+        if (map_get(m, label) != NULL) {
              return NULL; // should signal error for duplicate labels
         }
         map_add(m, label, int_to_bin16(rom_address));
 
     } else {
-        c->type = C_COMMAND;
+        c->type = C_INSTRUCTION;
         char* dest = malloc(sizeof(char) * 4);
         char* comp = malloc(sizeof(char) * 4);
         char* jump = malloc(sizeof(char) * 4);
@@ -322,17 +321,17 @@ Command* parse_line (char line[], Map m) {
                 jump[i-1] = *(jmp_ptr + i);
             }
         }
-        if(!comp_ptr) {
+        if (!comp_ptr) {
             // we dont have dest
             i = 0;
-            while(line[i] != ';'){
+            while(line[i] != ';') {
                 comp[i] = line[i];
                 i++;
             }
         } else if (jmp_ptr) {
             // we have both dest and jump
             i = 1;
-            while ((comp_ptr + i) != jmp_ptr){
+            while ((comp_ptr + i) != jmp_ptr) {
                 comp[i] = *(comp_ptr + i);
                 i++;
             }
@@ -354,11 +353,11 @@ Command* parse_line (char line[], Map m) {
         c->data.c_fields.dest = dest;
         rom_address++;
     }
-
    return c;
 }
 
-char* get_destination_filename(char* source_filename) {
+char* get_destination_filename(char* source_filename)
+{
     size_t substr_len = strlen(source_filename) - 3;
     char* dest = malloc(substr_len + 5);
     memcpy(dest, source_filename, substr_len);
@@ -366,18 +365,19 @@ char* get_destination_filename(char* source_filename) {
     return dest;
 }
 
-int get_instruction(char* line, FILE* fptr) {
+int get_instruction(char* line, FILE* fptr)
+{
     int i = 0;
-    line[i] = '\0'; // empty string
+    line[i] = '\0';
     char c;
     while (1)
     {
-        c=fgetc(fptr);
+        c = fgetc(fptr);
         if (c == EOF) return -1;
         if (c == '/' && i > 0 && line[i-1] == '/')
         {
             line[--i] = '\0';
-            while((c = fgetc(fptr)) != '\n' && c != EOF){} // go to the end of the line
+            while((c = fgetc(fptr)) != '\n' && c != EOF) {}
             return c==EOF ? -1: 1;
         }
         else if (c == '\n')
@@ -398,31 +398,30 @@ int main (int argc, char** argv)
     t = clock();
     char* source_filename = argv[1];
     FILE* fptr = fopen(source_filename,"r");
-    if (fptr == NULL)
-    {
+    if (fptr == NULL) {
         printf("File '%s' does not exist.\n", source_filename);
         exit(1);
     }
-    C_NODE* head = NULL;
+    c_node* head = NULL;
     Map m = new_map();
     char* dest_filename = get_destination_filename(source_filename);
     char c, line[MAX_LINE];
     int i = 0;
-    Command* cmd = NULL;
+    instruction* instr = NULL;
 
     install_predefined_symbols(m);
     int should_continue;
 
     do {
         should_continue = get_instruction(line, fptr);
-        cmd = parse_line(line,m);
-        if (cmd != NULL) {
-            head = list_add(head,cmd);
+        instr = parse_line(line,m);
+        if (instr != NULL) {
+            head = list_add(head,instr);
         }
     } while (should_continue != -1);
 
     fclose(fptr);
-    print_commands(head);
+    print_instructions(head);
     assemble(head, m, dest_filename);
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
@@ -430,42 +429,43 @@ int main (int argc, char** argv)
     return 0;
 }
 
-void write_instruction(FILE *fptr, char* command){
-    fprintf(fptr, "%s\n", command);
+void write_instruction(FILE *fptr, char* instruction)
+{
+    fprintf(fptr, "%s\n", instruction);
 }
 
-void assemble(C_NODE* head, Map m, char* dest) {
-    C_NODE* temp = head;
+void assemble(c_node* head, Map m, char* dest)
+{
+    c_node* temp = head;
     FILE* fptr = fopen(dest, "w");
-    char* instruction = NULL;
+    char* bin_instr = NULL;
     while (temp != NULL) {
-        Command* cmd = temp->cmd;
-        switch(cmd->type) {
-            case A_COMMAND:
-                instruction = cmd->a_cmd_mode == A_SYMBOL ? map_get(m, cmd->data.symbol) : int_to_bin16(cmd->data.value);
-                if (instruction == NULL) {
-                    instruction = int_to_bin16(ram_address++);
-                    map_add(m, cmd->data.symbol, instruction);
+        instruction* instr = temp->instr;
+        switch(instr->type) {
+            case A_INSTRUCTION:
+                bin_instr = instr->a_instr_mode == A_SYMBOL ? map_get(m, instr->data.symbol) : int_to_bin16(instr->data.value);
+                if (bin_instr == NULL) {
+                    bin_instr = int_to_bin16(ram_address++);
+                    map_add(m, instr->data.symbol, bin_instr);
                 }
-                write_instruction(fptr, instruction);
+                write_instruction(fptr, bin_instr);
                 break;
-            case C_COMMAND:
-                instruction = malloc(sizeof(char) * 17);
-                instruction[0] = instruction[1] = instruction[2] = '1';
-                memcpy(&instruction[3], get_comp_inst(cmd->data.c_fields.comp), 7 * sizeof(char));
-                memcpy(&instruction[10], get_dest_inst(cmd->data.c_fields.dest), 3 * sizeof(char));
-                memcpy(&instruction[13], get_jmp_inst(cmd->data.c_fields.jump), 3 * sizeof(char));
-                instruction[16] = '\0';
-                write_instruction(fptr, instruction);
+            case C_INSTRUCTION:
+                bin_instr = malloc(sizeof(char) * 17);
+                bin_instr[0] = bin_instr[1] = bin_instr[2] = '1';
+                memcpy(&bin_instr[3], get_comp_inst(instr->data.c_fields.comp), 7 * sizeof(char));
+                memcpy(&bin_instr[10], get_dest_inst(instr->data.c_fields.dest), 3 * sizeof(char));
+                memcpy(&bin_instr[13], get_jmp_inst(instr->data.c_fields.jump), 3 * sizeof(char));
+                bin_instr[16] = '\0';
+                write_instruction(fptr, bin_instr);
                 break;
             case LABEL:
-                // do nothing
                 break;
         }
-        if (instruction != NULL) {
-            free(instruction);
+        if (bin_instr != NULL) {
+            free(bin_instr);
+            bin_instr = NULL;
         }
-        instruction = NULL;
         temp=temp->next;
     }
     fclose(fptr);
